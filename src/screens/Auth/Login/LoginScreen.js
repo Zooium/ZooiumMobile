25059bf7750cb17d40cc778aad42b4b3d178e2d5
ZapsterStudios <andreas@zapsterstudios.com>
2019-09-24 11:47:22 +0200

    Improve login screen keyboard avoiding

--- src/screens/Auth/Login/LoginScreen.js
+++ src/screens/Auth/Login/LoginScreen.js
@@ -2,9 +2,8 @@ import React from 'react';
 import i18n from '@src/i18n.js';
 import theme from '@src/theme.js'
 import SafeView from '@components/SafeView.js';
-import { Image, StyleSheet } from 'react-native';
+import { View, Image, ScrollView, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { Text, Input, Button, Layout } from 'react-native-ui-kitten';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default class LoginScreen extends React.Component {
     state = {
@@ -20,74 +19,71 @@ export default class LoginScreen extends React.Component {
     }
 
     render = () => (
-        <SafeView style={styles.background}>
-            <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }}> 
-                <Layout style={styles.container}>
-                    <Layout style={styles.brand}>
-                        <Image style={styles.logo} source={require('@assets/icon.png')} />
-                        <Text category="h3" style={styles.brandText}>Zooium</Text>
-                    </Layout>
+        <Layout style={s.background}>
+            <SafeView style={{ flex: 1 }}>
+                <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+                    <ScrollView contentContainerStyle={s.scroll}>
+                        <View style={s.brand}>
+                            <Image style={s.logo} source={require('@assets/icon.png')} />
+                            <Text category="h3" style={s.brandText}>Zooium</Text>
+                        </View>
 
-                    <Layout style={styles.form}>
-                        <Input
-                            value={this.state.username}
-                            returnKeyType="next"
-                            blurOnSubmit={false}
-                            onChangeText={username => this.setState({ username })}
-                            onSubmitEditing={() => this.password.current.focus()}
-                            size="large"
-                            textContentType="username"
-                            placeholder={i18n.t('Username')}
-                            style={styles.input}
-                        />
-                        
-                        <Input
-                            ref={this.password}
-                            value={this.state.password}
-                            onChangeText={password => this.setState({ password })}
-                            onSubmitEditing={this.submit}
-                            size="large"
-                            textContentType="password"
-                            placeholder={i18n.t('Password')}
-                            secureTextEntry={true}
-                        />
-                    </Layout>
+                        <View style={s.form}>
+                            <Input
+                                value={this.state.username}
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                                onChangeText={username => this.setState({ username })}
+                                onSubmitEditing={() => this.password.current.focus()}
+                                size="large"
+                                textContentType="username"
+                                placeholder={i18n.t('Username')}
+                                style={s.input}
+                            />
+                            
+                            <Input
+                                ref={this.password}
+                                value={this.state.password}
+                                onChangeText={password => this.setState({ password })}
+                                onSubmitEditing={this.submit}
+                                size="large"
+                                textContentType="password"
+                                placeholder={i18n.t('Password')}
+                                secureTextEntry={true}
+                                style={s.input}
+                            />
 
-                    <Button status="white" size="giant" onPress={this.submit} style={styles.submit}>
-                        {i18n.t('Login')}
-                    </Button>
-                </Layout>
-            </KeyboardAwareScrollView>
-        </SafeView>
+                            <Button status="white" size="giant" onPress={this.submit}>
+                                {i18n.t('Login')}
+                            </Button>
+                        </View>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </SafeView>
+        </Layout>
     );
 }
 
-let styles = StyleSheet.create({
+let s = StyleSheet.create({
     background: {
+        flex: 1,
         backgroundColor: theme['color-primary-500'],
     },
 
-    container: {
-        flex: 1,
-        alignContent: 'space-between',
-        backgroundColor: theme['color-primary-500'],
+    scroll: {
+        flexGrow: 1,
+        justifyContent: 'space-between',
     },
 
     brand: {
         padding: '5%',
         alignItems: 'center',
-        backgroundColor: theme['color-primary-500'],
     },
 
     form: {
         flex: 1,
         padding: '5%',
         justifyContent: 'center',
-        backgroundColor: theme['color-primary-500'],
-    },
-
-    submit: {
-        borderRadius: 0,
     },
 
     logo: {
