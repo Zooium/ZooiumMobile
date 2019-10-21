d87944b594aadcb53e2410600a71f55b8a543686
ZapsterStudios <andreas@zapsterstudios.com>
2019-10-21 19:34:53 +0200

    Improved animal row rendering

--- src/screens/General/Animals/ListAnimalsScreen.js
+++ src/screens/General/Animals/ListAnimalsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import i18n from '@src/i18n.js';
 import { View } from 'react-native';
+import SexPreview from './components/SexPreview.js';
 import { Text, Layout } from 'react-native-ui-kitten';
 import ResourceList from '@components/ResourceList.js';
 import LIST_ANIMALS from '@graphql/queries/Animal/listAnimals.gql.js'
@@ -14,9 +15,28 @@ export default function ListAnimalsScreen() {
 
     preview = ({ item }) => {
         return (
-            <View>
-                <Text category="h6">{ item.name || item.identifier || '(' + i18n.t('name not set') + ')' }</Text>
-                <Text>{ specieText(item.specie) }</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <View style={{ flex: 1 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <SexPreview sex={item.sex} size={20} style={{marginRight: 10}} />
+
+                        <Text category="h6">
+                            { item.name || item.identifier || '(' + i18n.t('name not set') + ')' }
+                        </Text>
+                    </View>
+
+                    <Text>{ specieText(item.specie) }</Text>
+                </View>
+
+                {item.enclosure &&
+                    <View style={{ flexShrink: 0, alignItems: 'flex-end' }}>
+                        <Text style={{ fontWeight: 'bold' }}>
+                            {i18n.t('Enclosure', { count: 1 })}
+                        </Text>
+                        
+                        <Text>{ item.enclosure.name }</Text>
+                    </View>
+                }
             </View>
         );
     }
---
+++ src/screens/General/Animals/components/SexPreview.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import theme from '@src/theme.js';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+export default function SexPreview(props) {
+    const sex = {
+        male: {
+            icon: 'mars',
+            color: theme['color-primary-500'],
+        },
+
+        female: {
+            icon: 'venus',
+            color: '#CC6594',
+        },
+
+        unknown: {
+            icon: 'genderless',
+            color: theme['color-basic-500'],
+        },
+    }[props.sex || 'unknown'];
+
+    return (
+        <FontAwesome5 name={sex.icon} color={sex.color} {...props} />
+    );
+}
