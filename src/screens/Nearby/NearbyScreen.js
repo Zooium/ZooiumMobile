6dfb3cdb87b66b81d29c2f5f6eb4fe7793944286
ZapsterStudios <andreas@zapsterstudios.com>
2019-10-22 14:28:18 +0200

    Added location service to nearby tab

--- src/screens/Nearby/NearbyScreen.js
+++ src/screens/Nearby/NearbyScreen.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import * as Location from 'expo-location';
+import { Text, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { NavigationEvents } from 'react-navigation';
 
-export default class NearbyScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>NearbyScreen</Text>
-                <Button title="View Animal" onPress={() => this.props.navigation.navigate('ViewAnimal')} />
-                <Button title="View Enclosure" onPress={() => this.props.navigation.navigate('ViewEnclosure')} />
-            </View>
-        );
+export default function NearbyScreen() {
+    const [status, setStatus] = useState('undetermined');
+    const [watcher, setWatcher] = useState(undefined);
+    const [location, setLocation] = useState(undefined);
+
+    requestPermission = async () => {
+        // Request the user permission to location services.
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        setStatus(status);
+
+        // Returns whether or not request was granted.
+        return status === 'granted';
     }
+
+    boot = async() => {
+        // Request permission from user.
+        if (await requestPermission()) {
+            // Start location watcher service.
+            let service = await Location.watchPositionAsync({
+                accuracy: Location.Accuracy.BestForNavigation,
+                timeInterval: 500,
+                distanceInterval: 0,
+            }, (location) => setLocation(location));
+
+            // Save the watcher service.
+            setWatcher(service);
+        }
+    }
+
+    cleanup = () => {
+        // Remove location watcher if set.
+        if (watcher) {
+            watcher.remove();
+        }
+    }
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <NavigationEvents onWillFocus={boot} onDidBlur={cleanup} />
+
+            <Text>Status: {status}</Text>
+            <Text>Location: {location && location.coords.longitude}, {location && location.coords.latitude}</Text>
+        </View>
+    );
 }
