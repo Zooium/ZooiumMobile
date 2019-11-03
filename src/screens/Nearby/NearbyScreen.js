72c57717316e2dd48c3f1117501c8a3819ef4ed5
ZapsterStudios <andreas@zapsterstudios.com>
2019-11-03 23:12:33 +0100

    Added titles to nearby navbar toggle

--- src/screens/Nearby/NearbyScreen.js
+++ src/screens/Nearby/NearbyScreen.js
@@ -1,4 +1,6 @@
-import { View } from 'react-native';
+import i18n from '@src/i18n.js';
+import { View, Text } from 'react-native';
+import { Icon } from 'react-native-ui-kitten';
 import React, { useState, useEffect } from 'react';
 import BarcodeNearby from './components/BarcodeNearby.js';
 import LocationNearby from './components/LocationNearby.js';
@@ -23,13 +25,34 @@ NearbyScreen.navigationOptions = ({ navigation }) => {
     const isBarcode = view && view === 'barcode';
 
     return {
+        headerTitleStyle: {
+            flex: 1,
+            textAlign: 'left',
+        },
+
+        headerTitleContainerStyle: {
+            left: 0,
+        },
+    
         headerRight: (
             <HeaderButtons>
                 <Item
-                    title={isBarcode ? 'scan' : 'location'}
-                    iconName={isBarcode ? 'location-arrow' : 'qrcode'}
+                    title="toggle"
                     style={{ marginRight: 10 }}
                     onPress={() => navigation.getParam('setView')(isBarcode ? 'location' : 'barcode')}
+                    ButtonElement={
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={{
+                                color: 'white',
+                                fontSize: 16,
+                                marginRight: 14,
+                            }}>
+                                {i18n.t(isBarcode ? 'Use location' : 'Scan code')}
+                            </Text>
+
+                            <Icon color="white" size={20} name={isBarcode ? 'location-arrow' : 'qrcode'} />
+                        </View>
+                    }
                 />
             </HeaderButtons>
         ),
