008f76223fa3bb8f6d873778d7b82a96dcacf354
ZapsterStudios <andreas@zapsterstudios.com>
2019-11-04 19:22:54 +0100

    Added icon to empty resource list

--- src/components/resource/ResourceListEmpty.js
+++ src/components/resource/ResourceListEmpty.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import i18n from '@src/i18n.js';
+import theme from '@src/theme.js';
 import { View } from 'react-native';
-import { Text } from 'react-native-ui-kitten';
+import { Text, Icon } from 'react-native-ui-kitten';
 
 export default ({ resource }) => {
     return (
-        <View style={{
-            alignItems: 'center',
-            paddingVertical: 12,
-        }}>
-            <Text>
+        <View style={{ alignItems: 'center', paddingVertical: 12 }}>
+            <Icon name="binoculars" size={75} color={theme['color-basic-500']} />
+
+            <Text style={{ marginTop: 20 }}>
                 {i18n.t('No {{resource}} matched the given criteria.', { resource })}
             </Text>
         </View>
