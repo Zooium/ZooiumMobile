a153575d9a61bef968c604fa05c6667eccb3c5e1
ZapsterStudios <andreas@zapsterstudios.com>
2020-01-15 18:05:07 +0100

    Made map view go to user location by default

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,25 +1,58 @@
+import * as Location from 'expo-location';
 import { withNavigation } from 'react-navigation';
 import { default as Map, Marker } from 'react-native-maps';
 import React, { Fragment, useRef, useEffect } from 'react';
 import MapViewExpander from '@components/MapViewExpander.js';
 
 function MapView({ latitude, longitude, setCoordinates, editable = false, isFullscreen = false, style, navigation }) {
+    // Hold map view reference.
     const mapRef = useRef(null);
 
-    const handleEvent = ({ nativeEvent: { coordinate } }) => {
-        // Set coordinates on parent.
-        setCoordinates(coordinate.latitude, coordinate.longitude);
-    };
-
+    // Animate to coordinates on change.
     useEffect(() => {
         // Animate camera to new coordinates.
-        if (mapRef && mapRef.current) {
+        if (mapRef && mapRef.current && latitude && longitude) {
             mapRef.current.animateCamera({
                 center: { latitude, longitude },
             }, 1);
         }
     }, [latitude, longitude]);
 
+    // Go to user coordinates if editing new.
+    useEffect(() => {
+        // Skip if not editable or already has locations.
+        if (! editable || latitude && longitude) return;
+
+        // Create and call async function.
+        (async () => {
+            // Ask user for location permissions.
+            const response = await Location.requestPermissionsAsync();
+            if (! response.granted) return;
+
+            // Get users last known location.
+            const location = await Location.getLastKnownPositionAsync();
+            if (! location || ! location.coords) return;
+
+            // Extract coordinate data from location. 
+            const { coords: { latitude, longitude, altitude } } = location;
+
+            // Set camera to new coordinates.
+            if (mapRef && mapRef.current) {
+                mapRef.current.setCamera({
+                    zoom: 16,
+                    altitude: altitude+500,
+                    center: { latitude, longitude },
+                });
+            }
+        })();
+    }, []);
+
+    // Set coordinates on parent on edit event.
+    const handleEvent = ({ nativeEvent: { coordinate } }) => {
+        setCoordinates(coordinate.latitude, coordinate.longitude);
+    };
+
+    // Return map view.
     return (
         <Fragment>
             <Map
